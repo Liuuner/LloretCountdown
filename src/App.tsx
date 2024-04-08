9af554c030f09ac3e060039f3d0d6970a66a406f
liun <liun@tappolet.ch>
2024-04-08 08:28:15 +0200

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
 import './App.css';
 import FlipClockCountdown, {createDate} from "./components/FlipClockCountdown";
+import {useState} from "react";
+
 
 function App() {
-    // YYYY-MM-DDTHH:mm:ss.sssZ
-    const target = createDate(2024, 7, 16, 7, 0)
+    // const [target, setTarget] = useState<Date>(createDate(2024, 4, 2, 15, 11));
+    const [target, setTarget] = useState<Date>(createDate(2024, 7, 16, 7, 0));
+    const [isComplete, setIsComplete] = useState(false);
+
+    const onComplete = () => {
+        setIsComplete(true);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    window.setTarget = setTarget;
 
     return (
         <>
-            <h1>Countdown to Lloret</h1>
-            <FlipClockCountdown to={target}/>
+            <main className={isComplete ? "complete" : "incomplete"}>
+                <h1>Countdown to Lloret</h1>
+                <FlipClockCountdown
+                    to={target}
+                    dividerStyle={{color: "#FFF"}}
+                    digitBlockStyle={{backgroundColor: "var(--secondary)", fontWeight: 600}}
+                    separatorStyle={{color: "var(--secondary)"}}
+                    labelStyle={{color: "#000"}}
+                    onComplete={onComplete}
+                    hideOnComplete={false}
+                />
+            </main>
+
+            <div id={"drop_gradient"}/>
+            <div id="drop_pattern"/>
         </>
+
     )
 }
 
